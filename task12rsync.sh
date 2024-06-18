02c209f4478b655cca104177a8faeb81aa722bd8
Fahrizal Fahmi <fahrizalfahmi44@gmail.com>
2024-06-18 22:20:00 +0700

    Add files via upload

---
+++ task12rsync.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <source_file> <username> <ip_address>"
+    exit 1
+fi
+
+source_file="$1"
+username="$2"
+ip_address="$3"
+
+if [ ! -f "$source_file" ]; then
+    echo "Error: Source file '$source_file' not found."
+    exit 1
+fi
+
+echo "Copying $source_file to $username@$ip_address:~/..."
+rsync -avz --progress "$source_file" "$username@$ip_address":~/
+
+if [ $? -eq 0 ]; then
+    echo "File copied successfully using rsync."
+else
+    echo "Failed to copy file using rsync."
+fi
